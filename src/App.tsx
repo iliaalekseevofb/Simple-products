2962b764e7c26901eff65cc6d2e8db9c82b5e713
iliaalekseevofb <iliaalekseevofb@gmail.com>
2023-03-17 04:11:50 +0300

    New product is added, mock data is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Product from "./components/product";
+import Product from "./components/Product";
+import { products } from "./data/products";
 
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product />
+      <Product product={products[0]}/>
+      <Product product={products[1]}/>
     </div>
   )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,18 @@
-function Product() {
+import { IProduct } from "../models";
+
+interface ProductsProps {
+  product: IProduct
+}
+
+function Product({ product }: ProductsProps) {
+  console.log(product);
+
   return (
-    <div className="text-orange-500">
-      Product
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img className="w-1/6" src={product.image} alt={product.title}/>
+      <p>{product.title}</p>
+      <p className="font-bold">{product.price}</p>
+      <p>{product.description}</p>
     </div>
   )
 }
