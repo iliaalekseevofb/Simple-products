cf173f04a9ed16e978aa7b7c38e9d2d66cb7194e
iliaalekseevofb <iliaalekseevofb@gmail.com>
2023-03-18 15:07:48 +0300

    Toggle button, images and description are added

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IProduct } from "../models";
 
 interface ProductsProps {
@@ -5,14 +6,23 @@ interface ProductsProps {
 }
 
 function Product({ product }: ProductsProps) {
-  console.log(product);
+  const [isDetailsShowed, setIsDetailsShowed] = useState(false);
+  const btnClassName = isDetailsShowed ? 'bg-blue-400' : 'bg-yellow-400';
 
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       <img className="w-1/6" src={product.image} alt={product.title}/>
       <p>{product.title}</p>
       <p className="font-bold">{product.price}</p>
-      <p>{product.description}</p>
+      <button
+        className={`py-2 px-4 border ${btnClassName}`}
+        onClick={() => setIsDetailsShowed(prev => !prev)}
+      >
+        {isDetailsShowed ? 'Hide details' : 'Show details'}
+      </button>
+      {isDetailsShowed &&
+        <p>{product.description}</p>
+      }
     </div>
   )
 }
